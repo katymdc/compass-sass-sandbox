352be08e9ec7906233ad77fdd0dfcb0cdf003d3a
katymdc <katymdc@gmail.com>
2014-10-29 12:10:25 -0600

    Changed to portfolio site

--- README.md
+++ README.md
@@ -3,12 +3,15 @@ Notes for new environment:
 Need to install Node JS (grunt), Ruby (gems compass susy)
 
 Terminal instructions
-Go to project directory (folder with package.js)
+Go to project directory (folder with package.json)
 
+
+Note: on Windows you may need to create an npm folder in the appData/Roaming folder as well as run console as admin
 npm install
 (sudo) npm install -g grunt-cli
 
 For Ruby
 sudo gem install compass
 sudo gem install susy
+sudo gem install breakpoint
 
---
+++ contactthanks.php
@@ -0,0 +1,30 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Strict//EN">
+
+<head>
+	<title>Mad Dino Coding | You're Hiring Me! Hopefully.</title>
+	
+	<link rel="stylesheet" type="text/css" href="css/style.css" />
+</head>
+
+<body>
+
+	<div id="page-wrap">
+
+		
+		
+		<h1>Your message has been sent! I'll get back to you shortly.</h1><br />
+		
+		<p><a href="index.html">Back to Contact Form</a></p>
+	
+	</div>
+	
+	<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+	</script>
+	<script type="text/javascript">
+	_uacct = "UA-53373330-1";
+	urchinTracker();
+	</script>
+
+</body>
+
+</html>
